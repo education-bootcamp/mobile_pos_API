1b132c62f932d6ab4b7decd0189e281b03557e04
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2026-02-08 11:10:00 +0530

    init

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -56,7 +56,6 @@ const getOrder = async (req, res)=>{
 const createOrder = async (req, res)=>{
     try{
 
-
         const {customer, productDetails}=req.body;
 
         let totalAmount =0;
@@ -87,8 +86,6 @@ const createOrder = async (req, res)=>{
             await product.save();
         }
 
-
-
         const order = await Order.create({
             customer,
             productDetails,
@@ -102,11 +99,125 @@ const createOrder = async (req, res)=>{
 
         res.status(201).json({
             sucecss:true,
-            message:populatedOrder
+            data:populatedOrder
         });
        
     }catch(error){
         res.status(400).json({
+            success:false,
+            error:error.message
+        })
+    }
+}
+
+// @desc Update Order
+// @route PUT /api/v1/orders/:id
+// @access Private
+
+const updateOrder = async (req, res)=>{
+    try{
+
+        let order = await Order.findById(req.params.id);
+
+        if(!order){
+            res.status(404).json({
+            sucecss:false,
+            message:'Order not found'
+            });
+        }
+
+        // if updating product details, recalculate stock and total
+        if(req.body.productDetails){
+            for(let item of order.productDetails){
+                const product = await Product.findById(item.product);
+                product.qtyOnHand+=item.quantity;
+                await product.save();
+            }
+
+            let totalAmount =0;
+            for(let item of req.body.productDetails){
+                const product = await Product.findById(item.product);
+
+                if(!product){
+                    return res.status(404).json({
+                        sucecss:false,
+                        message:`Product with id ${item.product} not found`
+                    });
+                }
+
+                if(product.qtyOnHand<item.quantity){
+                return res.status(400).json({
+                        sucecss:false,
+                        message:`insufficiant quantity for product ${product.description}`
+                    });
+                }
+
+                item.price=product.unitPrice;
+                totalAmount+=product.unitPrice*item.quantity;
+
+                   // update product quantity
+                    product.qtyOnHand-=item.quantity;
+                    await product.save();
+            }
+
+            req.body.totalAmount = totalAmount;
+
+        }
+
+         order  = await Order.findByIdAndUpdate(req.params.id, req.body,{new:true, runValidators:true})
+        .populate('customer', 'name address')
+        .populate('productDetails.product', 'description unitPrice');
+
+        res.status(200).json({
+            sucecss:true,
+            data:order
+        });
+
+    }catch(error){
+        res.status(400).json({
+            success:false,
+            error:error.message
+        })
+    }
+}
+
+// @desc Delete Order
+// @route DELETE /api/v1/orders/:id
+// @access Private
+
+const deleteOrder = async (req, res)=>{
+    try{
+
+        let order = await Order.findById(req.params.id);
+
+        if(!order){
+            res.status(404).json({
+            sucecss:false,
+            message:'Order not found'
+            });
+        }
+
+        // Restore Product quantities
+
+        for(let item of order.productDetails){
+                const product = await Product.findById(item.product);
+
+                if(product){
+                    product.qtyOnHand += item.quantity;
+                    await product.save();
+                }
+            }
+
+            await Order.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            sucecss:true,
+            data:{},
+            message:'Order Deleted'
+        });
+        
+    }catch(error){
+        res.status(500).json({
             success:false,
             error:error.message
         })
